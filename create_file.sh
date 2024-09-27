1e3bb178174bcefed547568b42aa2ce92fe33830
Pavithra952 <vbtspavithra@gmail.com>
2024-09-27 08:25:11 +0000

    Add script files to cloud training

---
+++ create_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#create a new file named my_file.sh
+touch my_file.sh
+#add some message to the file
+echo "this is simple text file" > my_file.sh
+#display the message
+echo "file created and text added"
+
